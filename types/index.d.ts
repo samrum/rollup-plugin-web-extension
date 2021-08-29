3829c9ac4a33ce612a13f557005003bbefc618d3
Ruben Medina <dev@rubenmedina.com>
2021-08-29 16:42:50 -0700

    refactor: use real manifest type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,24 +1,6 @@
 import type { Plugin } from "rollup";
 
-interface WebExtensionManifest {
-  version: string;
-  name: string;
-  description?: string;
-  background?: {
-    page?: string;
-    persistent?: boolean;
-  };
-  content_scripts?: {
-    js?: string[];
-    css?: string[];
-    matches: string[];
-  }[];
-  browser_action?: {
-    default_popup?: string;
-  };
-  web_accessible_resources?: string[];
-  manifest_version: number;
-}
+type WebExtensionManifest = chrome.runtime.Manifest;
 
 interface RollupWebExtensionOptions {
   /**
