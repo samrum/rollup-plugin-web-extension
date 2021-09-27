c708a854c152dd0a8a44ef84a78f373eac891a79
Ruben Medina <dev@rubenmedina.com>
2021-09-27 00:13:50 -0700

    chore: tweak project description

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,13 +4,13 @@ type WebExtensionManifest = chrome.runtime.Manifest;
 
 interface RollupWebExtensionOptions {
   /**
-   * The manifest file to use as a base for the generated manifest
+   * The manifest file to use as a base for the generated extension
    */
   manifest: WebExtensionManifest;
 }
 
 /**
- * Build cross platform web extensions using rollup
+ * Build cross platform, module-based web extensions using rollup
  */
 export default function webExtension(
   options?: RollupWebExtensionOptions
