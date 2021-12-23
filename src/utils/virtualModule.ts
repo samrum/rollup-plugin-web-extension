b2997153c42a8161a1930e33c63cb349f8211741
Ruben Medina <dev@rubenmedina.com>
2021-12-22 22:32:58 -0800

    refactor: use a prepend with virtual modules

--- src/utils/virtualModule.ts
+++ src/utils/virtualModule.ts
@@ -3,9 +3,9 @@
 const virtualModules = new Map<string, string>();
 
 export function setVirtualModule(id: string, source: string) {
-  virtualModules.set(id, source);
+  virtualModules.set(`\0${id}`, source);
 }
 
 export function getVirtualModule(id: string): string | null {
-  return virtualModules.get(id) ?? null;
+  return virtualModules.get(`\0${id}`) ?? null;
 }
