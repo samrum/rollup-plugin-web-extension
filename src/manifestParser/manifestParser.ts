e7825164da88a0aab6dad9cb92567ca9f7aa2cd7
Ruben Medina <dev@rubenmedina.com>
2021-12-30 22:14:32 -0800

    docs: explain interface method purposes a bit

--- src/manifestParser/manifestParser.ts
+++ src/manifestParser/manifestParser.ts
@@ -4,10 +4,13 @@ import type { Manifest as ViteManifest, ResolvedConfig } from "vite";
 export default interface ManifestParser<
   Manifest extends chrome.runtime.Manifest
 > {
+  // Pull files to emit and files to pass on to vite for processing from input manifest
   parseManifest(manifest: Manifest): Promise<ParseResult<Manifest>>;
 
+  // Write base files needed for the extension to function, update imports to pull from dev server
   writeServeBuild(manifest: Manifest, devServerPort: number): Promise<void>;
 
+  // Use vite manifest file to do things like add content script loaders, update extension manifest filenames
   parseViteManifest(
     viteManifest: ViteManifest,
     outputManifest: Manifest,
